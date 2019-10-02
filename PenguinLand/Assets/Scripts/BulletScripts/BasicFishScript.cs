5aa4c3d00ad871f90018a098f8861a716f8d19f2
ggdgg94 <ggdgg@protonmail.com>
2019-10-02 13:17:59 -0500

    basic demo tutorial stage

---
+++ PenguinLand/Assets/Scripts/BulletScripts/BasicFishScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/* Eventually we'll want to change this into a scriptable item
+ so this will be made into an abstract class  */
+public class BasicFishScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 4f;
+    private Vector2 tmpPos;
+    public float deactivateTimer = 4f;
+    void Start()
+    {
+        Invoke("DeactivateGameObject", deactivateTimer);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        
+    }
+
+    void Move(){
+        tmpPos = transform.position;
+        tmpPos.x += speed * Time.deltaTime;
+        transform.position = tmpPos;
+
+    }
+
+    void DeactivateGameObject(){
+        gameObject.SetActive(false);
+    }
+}
---
+++ PenguinLand/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 5f;
+    public float maxX, maxY, minX, minY;
+    private Vector2 tmpPos;
+    private int dirX, dirY;
+
+    //Fish shot will probably be moved to a Gun class
+    [SerializeField]
+    private GameObject fishShot;
+    [SerializeField]
+    private Transform shootPoint;
+
+    public float shootTimer = 0.35f;
+    private float currentTimer;
+    private bool canShoot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTimer = shootTimer;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        Shoot();
+        Override();
+    }
+
+    void MovePlayer(){
+        //change getaxisraw eventually to controller code or just better non-hardcoded code
+        dirX = Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
+        dirY = Mathf.RoundToInt(Input.GetAxisRaw("Vertical"));
+
+        tmpPos = transform.position;
+        tmpPos.x += dirX * speed * Time.deltaTime;
+        tmpPos.y += dirY * speed * Time.deltaTime;
+
+        if(tmpPos.x > maxX)
+            tmpPos.x = maxX;
+
+        if(tmpPos.x < minX)
+            tmpPos.x = minX;
+
+        if(tmpPos.y > maxY)
+            tmpPos.y = maxY;
+
+        if(tmpPos.y < minY)
+            tmpPos.y = minY;
+
+        transform.position = tmpPos;
+    }
+    //We'll use this later probably for sprite rotations and firing direction
+    void Rotate(){
+
+    }
+
+    //most of the logic here will probably moved to a Gun class
+    void Shoot(){
+        shootTimer += Time.deltaTime;
+        if(shootTimer > currentTimer){
+            canShoot = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.K)){
+            if(canShoot){
+                canShoot = false;
+                shootTimer = 0f;
+                Instantiate(fishShot, shootPoint.position, Quaternion.identity);
+            }
+            
+        }
+    }
+
+    //This one will probably be placed in a Gun script
+    void Override(){
+        if(Input.GetKeyDown(KeyCode.J)){
+            currentTimer = 0.15f;
+        }
+    }
+}
